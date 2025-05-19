c74e832df2fcee0668fb6eee5014dfc66a158ad2
dart-prac <mustafizur15-4365@diu.edu.bd>
2025-05-19 21:03:57 +0600

    Task mnager

--- lib/ui/screens/new_task_screen.dart
+++ lib/ui/screens/new_task_screen.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:task_manger/ui/screens/add_new_task_screen.dart';
+import 'package:task_manger/ui/widgets/task_summary_card.dart';
 
-class NewTaskScreen extends StatelessWidget {
+class NewTaskScreen extends StatefulWidget {
   const NewTaskScreen({super.key});
 
   @override
+  State<NewTaskScreen> createState() => _NewTaskScreenState();
+}
+
+class _NewTaskScreenState extends State<NewTaskScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('New'),
+    return Scaffold(
+      body: Column(
+        children: [
+          _buildSummarSection(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onTapAddFAB,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _buildSummarSection() {
+    return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                TaskSummaryCard(
+                  count: 9,
+                  tittle: 'New',
+                ),
+                TaskSummaryCard(
+                  count: 9,
+                  tittle: 'Completed',
+                ),
+                TaskSummaryCard(
+                  count: 9,
+                  tittle: 'Cancelled',
+                ),
+                TaskSummaryCard(
+                  count: 9,
+                  tittle: 'Progress',
+                ),
+              ],
+            ),
+          ),
+        );
+  }
+  void _onTapAddFAB() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => AddNewTaskScreen()),
     );
   }
 }
+
+
